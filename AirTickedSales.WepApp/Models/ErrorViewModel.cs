421ceafc58fef9b68c981e8f676ca5b4d63423e9
Tuan Anh <anhtuan20011997@gmail.com>
2020-11-26 16:36:55 +0700

    Add project files.

---
+++ AirTickedSales.WepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AirTickedSales.WepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
