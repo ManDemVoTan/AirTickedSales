3ceb7a9c43e7fad7e5801b157816286664bef023
Tuan Anh <anhtuan20011997@gmail.com>
2020-11-30 14:16:51 +0700

    using Microsoft.AspNetCore.Authentication.JwtBearer and   FluentValidation

---
+++ AirTickedSales.Application/Catalog/System/User/UserService.cs
@@ -0,0 +1,75 @@
+﻿using AirTickedSales.Data.Entities;
+using AirTickedSales.ViewModel.Catalog.System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTickedSales.Application.Catalog.System.User
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _singInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _config;
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> singInManager, RoleManager<AppRole> roleManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _singInManager = singInManager;
+            _roleManager = roleManager;
+            _config = config;
+
+        }
+        public async Task<string> AuthentiCate(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null)  return null;
+            var result = await _singInManager.PasswordSignInAsync(user, request.passWord, request.RememberMe, true);
+            if (!result.Succeeded) return null;
+            var roles = _userManager.GetRolesAsync(user);
+            var claims = new[]
+        {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName,user.FirstName),
+                new Claim(ClaimTypes.Role, string.Join(";",roles)),
+                new Claim(ClaimTypes.Name, request.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                _config["Tokens:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+                Dob = request.Dob,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                PhoneNumber = request.PhoneNUmber,
+            };
+            var result = await _userManager.CreateAsync(user, request.PassWord);
+            if(result.Succeeded)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
+
+
---
+++ AirTickedSales.ViewModel/Catalog/System/User/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AirTickedSales.ViewModel.Catalog.System
+{
+    public class RegisterRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public string Email { get; set; }
+        public string  PhoneNUmber { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string ConfirmPassWord { get; set; }
+
+    }
+}
